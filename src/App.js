30bd7f92d7ab1c8d9586181ea36d11d466d072ac
Miguel Alonso <malbonm07@gmail.com>
2020-11-19 13:50:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/home'
+import Navbar from './components/ui/Navbar'
 
+import About from './pages/about';
+import Todo from './pages/todo';
+// const About = lazy(() => import('./pages/about'));
+// const Todo = lazy(() => import('./pages/todo'));
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/todo">
+              <Todo />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+        </Switch>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
